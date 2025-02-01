548d61c230652f88bc36c27926b29ddd855f0ab8
Neideltern <neideltern@gmail.com>
2025-02-01 18:53:21 +0200

    shadcn/ui setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  server: {
+    host: '192.168.0.10',
+    port: 8000,
+  },
+  plugins: [vue(), vueDevTools()],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
