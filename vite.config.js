bb85fac85fd4facb4d6a123fbd4984b808747d39
Neideltern <neideltern@gmail.com>
2025-02-13 22:46:00 +0200

    Shadcn setup

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+
+// vue
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// shadcn-vue
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+
 // https://vite.dev/config/
 export default defineConfig({
+    server: {
+        host: '192.168.0.10',
+        port: 8000
+    },
+    css: {
+        postcss: {
+            plugins: [tailwind(), autoprefixer()],
+        },
+    },
     plugins: [vue(), vueDevTools()],
     resolve: {
         alias: {
